cd873ae91fcd307e48e65a759bd2fb9787933c57
junaidibiz <junaidibizid@gmail.com>
2025-06-17 02:24:15 +0700

    Membuat dan Mendaftarkan Global Mixins

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from "vue";
-import "./style.css";
 import App from "./App.vue";
 
 /**
@@ -8,10 +7,23 @@ import App from "./App.vue";
 import Toast from "vue-toastification";
 import "vue-toastification/dist/index.css";
 
+/**
+ * Tailwind CSS
+ */
+import "./style.css"; // Pastikan ini ada jika Anda mengikuti langkah instalasi Tailwind via @import di style.css
+
+/**
+ * Mixins
+ */
+import mixins from "./mixins"; // Import file mixins yang baru Anda buat
+
 //create App Vue
 const app = createApp(App);
 
 //gunakan "Toast" di Vue Js dengan plugin "use"
 app.use(Toast);
+
+//gunakan "Mixins" di Vue Js dengan plugin "mixin"
+app.mixin(mixins); // Daftarkan mixins secara global
 
 app.mount("#app");
