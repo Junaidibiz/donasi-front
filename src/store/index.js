6c879b0651649183d64e594e4344b67948b8bec8
junaidibiz <junaidibizid@gmail.com>
2025-06-18 11:43:08 +0700

    Konfigurasi Modul Donation Vuex

--- src/store/index.js
+++ src/store/index.js
@@ -4,9 +4,13 @@ import { createStore } from "vuex";
 //import module auth
 import auth from "./module/auth";
 
+//import module donation
+import donation from "./module/donation"; // <-- TAMBAHKAN IMPOR INI
+
 //create store vuex
 export default createStore({
   modules: {
-    auth, // <-- mendaftarkan module auth
+    auth, // <-- module auth
+    donation, // <-- TAMBAHKAN MODUL INI
   },
 });
---
+++ src/store/module/donation.js
@@ -0,0 +1,25 @@
+const donation = {
+  //set namespace true
+  namespaced: true,
+
+  //state
+  state: {
+    // State terkait donasi akan ditempatkan di sini
+  },
+
+  //mutations
+  mutations: {
+    // Mutasi untuk mengubah state donasi akan ditempatkan di sini
+  },
+
+  //actions
+  actions: {
+    // Aksi untuk melakukan operasi asinkron terkait donasi akan ditempatkan di sini
+  },
+
+  //getters
+  getters: {
+    // Getters untuk mendapatkan state donasi akan ditempatkan di sini
+  },
+};
+export default donation;
